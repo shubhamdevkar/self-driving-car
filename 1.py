0251e04e1d8a85bc6437766f0e3efcb916a59113
shubham devkar <shubhamdevkar@users.noreply.github.com>
2020-10-14 14:42:55 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+import cv2
+
+image = cv2.imread('test_image.jpg')
+cv2.imshow('results', image)
+cv2.waitKey(0)
+
+                   #this code read the image file and display
+                   #lets run
+                   #f5
+
